d9834cd7dc455fe9456df3c5827741e4a0065a7e
MAYSSA MARTHA FRANCISCO DA SILVA <21193395@CFP127ALUNO.LOCAL>
2021-09-14 16:34:42 -0300

    Finalizado html e css, agora é fazer as funções

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict"
+const semaforo = document.getElementById("semaforo")
+
+//Funções
+
+function luzVermelha(){
+    
+}
+
+function luzAmarela(){
+
+}
+
+function luzVerde(){
+
+}
+
+
+// Eventos
+document.getElementById("btnVermelho")
+    .addEventListener("click",luzVermelha)
+document.getElementById("btnAmarelo")
+    .addEventListener("click", luzAmarela)
+document.getElementById("btnVerde")
+    .addEventListener("click", luzVerde)
