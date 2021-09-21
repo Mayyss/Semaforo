b964cc8dee06ed190dcd07996d19f0153457751c
Mayssa <mayssasilva147@gmail.com>
2021-09-20 22:11:22 -0300

    Finalizado o semaforo

--- script.js
+++ script.js
@@ -1,25 +1,69 @@
 "use strict"
+//Criando constantes
 const semaforo = document.getElementById("semaforo")
+let idIntervalo
 
 //Funções
 
-function luzVermelha(){
-    
+function luzVermelho(){
+    semaforo.src = "img/vermelho.png"
 }
 
-function luzAmarela(){
-
+function luzAmarelo(){
+    semaforo.src = "img/amarelo.png"
 }
 
 function luzVerde(){
-
+    semaforo.src = "img/verde.png"
 }
 
+//Funções para intercalar as trocas de luzes
+function vermelhoLuz(){
+    return semaforo.src.includes ("vermelho")
+}
+
+function amareloLuz(){
+    return semaforo.src.includes ("amarelo")
+}
+
+//Função para parar as trocas de luzes
+function parar(){
+    clearInterval(idIntervalo)
+}
+
+//Função para trocar as luzes
+function trocarLuz(){
+    if(amareloLuz()){
+        luzVerde()
+    }
+    else if(vermelhoLuz()){
+        luzAmarelo()
+    }
+    else {
+        luzVermelho()
+    }
+}
+
+//Função que executa a troca e o parar
+function luzAutomatica(){
+    const auto = document.getElementById("btnAutomatico")
+    if (auto.textContent == "Automático"){
+        idIntervalo = setInterval(trocarLuz, 1000)
+        auto.textContent = "Parar"
+    }
+    else{
+        parar()
+        auto.textContent = "Automático"
+    }
+     
+}
 
 // Eventos
 document.getElementById("btnVermelho")
-    .addEventListener("click",luzVermelha)
+    .addEventListener("click",luzVermelho)
 document.getElementById("btnAmarelo")
-    .addEventListener("click", luzAmarela)
+    .addEventListener("click", luzAmarelo)
 document.getElementById("btnVerde")
     .addEventListener("click", luzVerde)
+document.getElementById("btnAutomatico")
+    .addEventListener("click", luzAutomatica)
